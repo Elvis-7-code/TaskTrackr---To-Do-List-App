f343fc2da30923bcdb61b430ec75436c320c4408
Elvis <elvis.wachira@student.moringaschool.com>
2025-04-25 13:23:26 +0300

    <TaskItem/>

--- src/Components/TaskList.jsx
+++ src/Components/TaskList.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import TaskItem from './TaskItem'
 
-const TaskList = () => {
+const TaskList = ({ tasks, deleteTask, editTask }) => {
   return (
-    <div>TaskList</div>
+    <div>
+      {tasks && tasks.map((task) => (
+        <TaskItem
+          key={task.id}
+          task={task}
+          deleteTask={deleteTask}
+          editTask={editTask}
+        />
+      ))}
+    </div>
   )
 }
 
