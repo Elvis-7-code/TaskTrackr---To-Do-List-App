ab72477c612169bd05f7c0b69c16f12798652d50
Elvis <elvis.wachira@student.moringaschool.com>
2025-04-25 12:58:26 +0300

    TaskForm

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,3 +1,10 @@
 function name(params) {
     const [tasks, setTasks] = useState([]);
+
+    return (
+        <div className="app">
+            <h1>TaskTrackr</h1>
+            <TaskForm tasks={tasks} deleteTask={...} editTask={...}/>
+        </div>
+    )
 }
