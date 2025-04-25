18eef8857bbd9b225e1bcf365e8c1d472f85a397
Elvis <elvis.wachira@student.moringaschool.com>
2025-04-25 13:35:02 +0300

    handleEdit

--- src/Components/TaskItem.jsx
+++ src/Components/TaskItem.jsx
@@ -3,8 +3,14 @@ import React from 'react'
 function TaskItem({task, deleteTask, editTask}) {
     const[isEditing, setIsEditing] = useState(false);
     const [text, setText] = useState(task.text);
-    
-const TaskItem = () => {
+
+const handleEdit= () => {
+    if (isEditing){
+        editTask(task.id, text);
+    }
+    setIsEditing(!isEditing);
+};
+
   return (
     <div>TaskItem</div>
   )
