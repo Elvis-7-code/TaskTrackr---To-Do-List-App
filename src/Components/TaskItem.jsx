14133ba85aed4f7d133bc370441b11e3209df9ee
Elvis <elvis.wachira@student.moringaschool.com>
2025-04-25 13:32:31 +0300

    TaskItem function

--- src/Components/TaskItem.jsx
+++ src/Components/TaskItem.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
+function TaskItem({task, deleteTask, editTask}) {
+    const[isEditing, setIsEditing] = useState(false);
+    const [text, setText] = useState(task.text);
+    
 const TaskItem = () => {
   return (
     <div>TaskItem</div>
